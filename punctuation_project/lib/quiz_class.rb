a7c1f6aed1ea01949436f2fa3e8a10e138d0442c
Sterling Walker <lazarus_star@hotmail.com>
2013-12-12 21:10:17 -0500

    Tallies score and allows retaking of quiz

--- punctuation_project/lib/quiz_class.rb
+++ punctuation_project/lib/quiz_class.rb
@@ -5,33 +5,12 @@ class Quiz
 
 attr_accessor :questions
 
-  @@correct_answers = 0
-  @@index = 0
-
-
 
   def initialize
   @questions = [{:name => "Electro", :type => "Villain"}, {:name =>"Octothorpe", :type => "Punctuation"}, {:name =>"Hedera", :type => "Punctuation"}]
     # More punctuation, "Chevron", "Caret", "Solidus", "Guillements", "Interrobang", "Pilcrow"]
   end
 
-  def tally
-    @@correct_answers
-  end
-
-  def clear_answers
-    @@correct_answers = 0
-    @@used_villains = []
-    @@used_punctuation = []
-  end
-
-  def index
-    @@index
-  end
-
-  def index=
-    @@index
-  end
 
 end
 
